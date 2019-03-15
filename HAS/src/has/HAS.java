df12721e661c70a68fc502cf9b01504bf202064f
Luis Tapia <36052812+14030598@users.noreply.github.com>
2019-03-15 08:52:46 -0600

    diseño del menu

--- HAS/src/has/HAS.java
+++ HAS/src/has/HAS.java
@@ -5,6 +5,9 @@
  */
 package has;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author luis
@@ -14,20 +17,50 @@ public class HAS {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Sintomas sintoma=new Sintomas();
+    public static void main(String[] args) throws IOException {
+        Scanner entrada= new Scanner(System.in);
         
-        //sintoma.insertar();
-        System.out.println("Lectura del archivo maestro:\n");
-        sintoma.leer_sintomas();
+        System.out.println("\t\tMenu principal");
+        System.out.println("*********************************************************");
+        System.out.println("*\t\t\t\t\t\t\t*\n*\t1.- Sintomas\t\t\t\t\t*\n*\t2.- Tratamientos\t\t\t\t*\n*\t3.- Causas Primarias\t\t\t\t*\n*\t4.- Causas Secundarias\t\t\t\t*\n*\t5.- Contraindicaciones\t\t\t\t*");
+        System.out.println("*\t\t\t\t\t\t\t*\n*********************************************************");
+        System.out.print("=============>\t");
+        int menu=entrada.nextInt();
+        System.out.println("\n\n\n\n\n\n");
+        System.out.println("*********************************************************");
+        System.out.println("\n\n\n\n\n\n");
+        switch(menu){
+            case 1:
+                Sintomas sintoma=new Sintomas();
+                System.out.println("\tElija la operacion a realizar");
+                System.out.println("*********************************************************");
+                System.out.println("\n\t1.- INSERTAR\n\t2.- MOSTRAR");
+                System.out.println("*\t\t\t\t\t\t\t*\n*********************************************************");
+                System.out.print("=============>\t");
+                int operacion=entrada.nextInt();
+                if (operacion==1) {
+                    sintoma.insertar();
+                }else
+                    if (operacion==2) {
+                        System.out.println("Lectura del archivo maestro:\n");
+                        sintoma.leer_sintomas();
+                    }
+                break;
+            default: 
+        }
+        System.out.println("*********************************************************");
+        
+        
+        
+        
         
         Tratamientos tratamiento = new Tratamientos();
-        tratamiento.insertar();
+        /*tratamiento.insertar();
         tratamiento.buscar();
-        tratamiento.getLlave();
+        tratamiento.getLlave();*/
         
         Contraindicaciones contraindicacion = new Contraindicaciones();
-        contraindicacion.buscar(farmaco);
+//        contraindicacion.buscar(farmaco);
     }
     
 }
